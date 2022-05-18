1cfc957d4f5747b08857bc0b7684aa824f67f980
vaishnaowankar <54058017+vaishnaowankar@users.noreply.github.com>
2022-05-18 23:36:35 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const app = express();
+const port = 3400;
+
+//to make pages .html .txt .jpeg .mp3 .mp4  static content 
+app.use(express.static("public"));
+
+app.listen(port, () => {
+    console.log(`app started on port ${port}`);
+})
+
